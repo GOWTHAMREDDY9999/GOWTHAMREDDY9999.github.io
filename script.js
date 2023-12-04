a1ff57bef9c52fa845d51d7ec6b45ff4ff27cd00
GOWTHAMREDDY9999 <66788459+GOWTHAMREDDY9999@users.noreply.github.com>
2023-12-04 16:16:07 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function startConversation() {
+    alert('Let\'s start chatting with ChatGPT!');
+    // You can add more interactive features here
+}
---
+++ styles.cs
@@ -0,0 +1,45 @@
+body {
+    font-family: 'Arial', sans-serif;
+    margin: 0;
+    padding: 0;
+}
+
+header {
+    background-color: #333;
+    color: #fff;
+    padding: 1em;
+}
+
+nav ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+
+nav ul li {
+    display: inline;
+    margin-right: 20px;
+}
+
+section {
+    margin: 20px;
+}
+
+.hero {
+    background-image: url('background-image.jpg');
+    background-size: cover;
+    color: #fff;
+    text-align: center;
+    padding: 50px;
+}
+
+video {
+    width: 100%;
+}
+
+footer {
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 1em;
+}
